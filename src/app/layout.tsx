e31e43153d64376a14868815b3f025bb04cf142f
daongoclinh27012001 <daongoclinh27012001@gmail.com>
2026-04-23 15:30:38 +0700

    Add files via upload

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'KOS SUPER CRM',
+  description: 'Không gian làm việc hiệu suất cho Bộ phận Tuyển dụng',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="vi">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
